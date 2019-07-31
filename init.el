ba1710a42f8f4e0150651c8f0b77d70bfe3f2ca1
Kevin Chang <changkevin@protonmail.ch>
2019-07-31 14:39:28 -0700

    Remove zweilight and add roguel-ike and cherry-blossom theme.

--- init.el
+++ init.el
@@ -25,6 +25,7 @@
 (add-to-list 'auto-mode-alist '("\\.l\\'" . flex-mode))
 (put 'downcase-region 'disabled nil)
 (global-auto-complete-mode)
+(load-theme 'cherry-blossom t) 
 
 ;; ExuberantCTags path.
 (setq path-to-etags
@@ -43,14 +44,42 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1b1a24" "#ee11dd" "#4c406d" "#b4f5fe" "#8CD0D3" "#DC8CC3" "#ffa500" "#8584ae"])
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("725a0ac226fc6a7372074c8924c18394448bb011916c05a87518ad4563738668" default)))
+    ("04589c18c2087cd6f12c01807eed0bdaa63983787025c209b89c779c61c3a4c4" "e9740103f6ae2cbc97fef889b85b1c51b4d4a2d95c2b398b57a1842d14d96304" "725a0ac226fc6a7372074c8924c18394448bb011916c05a87518ad4563738668" default)))
+ '(fci-rule-color "#4c406d")
  '(gotham-tty-256-colors t)
+ '(nrepl-message-colors
+   (quote
+    ("#ee11dd" "#8584ae" "#b4f5fe" "#4c406d" "#ffe000" "#ffa500" "#ffa500" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (zweilight-theme ac-etags auto-complete comment-dwim-2 poker gnugo chess web-mode go-mode gotham-theme golint fireplace abyss-theme))))
+    (roguel-ike cherry-blossom-theme ac-etags auto-complete comment-dwim-2 poker gnugo chess web-mode go-mode gotham-theme golint fireplace abyss-theme)))
+ '(vc-annotate-background "#0bafed")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#ee11dd")
+     (60 . "#8584ae")
+     (80 . "#ffe000")
+     (100 . "#efef80")
+     (120 . "#b4f5fe")
+     (140 . "#4c406d")
+     (160 . "#4c406d")
+     (180 . "#1b1a24")
+     (200 . "#4c406d")
+     (220 . "#65ba08")
+     (240 . "#ffe000")
+     (260 . "#ffa500")
+     (280 . "#6CA0A3")
+     (300 . "#0bafed")
+     (320 . "#8CD0D3")
+     (340 . "#ffa500")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ roguel-ike-saves/K.el
@@ -0,0 +1,1 @@
+("K" :human (:health 10 :stamina 10 :strength 5 :constitution 5 :speed 5 :spirit 5))
