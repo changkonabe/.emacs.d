2a2f013fdf85613a0d0831c6f021f5026b3b5f43
Kevin Chang <changkevin@protonmail.ch>
2019-08-01 14:46:50 -0700

    Add zone-when-idle to init.el, remove uninstalled package symbols, install ace-window.

--- init.el
+++ init.el
@@ -30,10 +30,7 @@
  '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("04589c18c2087cd6f12c01807eed0bdaa63983787025c209b89c779c61c3a4c4"
-     "e9740103f6ae2cbc97fef889b85b1c51b4d4a2d95c2b398b57a1842d14d96304"
-     "725a0ac226fc6a7372074c8924c18394448bb011916c05a87518ad4563738668"
-     default)))
+    ("04589c18c2087cd6f12c01807eed0bdaa63983787025c209b89c779c61c3a4c4" "e9740103f6ae2cbc97fef889b85b1c51b4d4a2d95c2b398b57a1842d14d96304" "725a0ac226fc6a7372074c8924c18394448bb011916c05a87518ad4563738668" default)))
  '(fci-rule-color "#4c406d")
  '(gotham-tty-256-colors t)
  '(nrepl-message-colors
@@ -41,7 +38,7 @@
     ("#ee11dd" "#8584ae" "#b4f5fe" "#4c406d" "#ffe000" "#ffa500" "#ffa500" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (roguel-ike cherry-blossom-theme ac-etags auto-complete comment-dwim-2 poker gnugo chess web-mode go-mode gotham-theme golint fireplace abyss-theme)))
+    (ace-window zone-rainbow zone-sl roguel-ike cherry-blossom-theme ac-etags auto-complete comment-dwim-2 web-mode go-mode gotham-theme golint fireplace)))
  '(vc-annotate-background "#0bafed")
  '(vc-annotate-color-map
    (quote
@@ -75,6 +72,7 @@
 (global-set-key "\C-xp" (lambda ()
                           (interactive)
                           (other-window -1)))
+(global-set-key (kbd "M-o") 'ace-window)
 (global-set-key "\M-;" 'comment-dwim-2)
 (put 'narrow-to-region 'disabled nil)
 (require 'flex-mode)
@@ -82,3 +80,6 @@
 (put 'downcase-region 'disabled nil)
 (global-auto-complete-mode)
 (load-theme 'cherry-blossom t)
+(show-paren-mode)
+(require 'zone)
+(zone-when-idle 120)
